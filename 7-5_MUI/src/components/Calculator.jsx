beb9ea67a141d9caeadf7794bf47e016942c605e
stevenwooden <161550247+stevenwooden@users.noreply.github.com>
2024-07-13 11:12:59 -0400

    completed all exercises

--- 7-5_MUI/src/components/Calculator.jsx
+++ 7-5_MUI/src/components/Calculator.jsx
@@ -1,5 +1,13 @@
 import {useState} from 'react'
 import Operation from './Operation';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 
 function Calculator(){
@@ -23,22 +31,62 @@ function Calculator(){
     return(
         <div>
             <div>
-                <input type='text' id='num' value={num1} placeholder='First Number' onChange={(e) => setNum1(e.target.value)} />
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+                >
+                <TextField id="outlined-basic" label="First Number" variant="outlined" onChange={(e) => setNum1(e.target.value)} />
+            </Box>
+                {/* <input type='text' id='num' value={num1} placeholder='First Number' onChange={(e) => setNum1(e.target.value)} /> */}
             </div>
             <div>
-                <select id='operation' value={operation} onChange={(e) => setOperation(e.target.value)}>
+            <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+
+                    <Select id='operation' value={operation} onChange={(e) => setOperation(e.target.value)}>
+                    <MenuItem value={'-'}>-</MenuItem>
+                    <MenuItem value={'+'}>+</MenuItem>
+                    <MenuItem value={'*'}>*</MenuItem>
+                    <MenuItem value={'/'}>/</MenuItem>
+                    </Select>
+                </FormControl>
+            </Box>
+                {/* <select id='operation' value={operation} onChange={(e) => setOperation(e.target.value)}>
                     <option value="+">+</option>
                     <option value="-">-</option>
                     <option value="*">*</option>
                     <option value="/">/</option>
-                </select>
+                </select> */}
             </div>
             <div>
-                <input type='text' id='num' value={num2} placeholder='Second Number' onChange={(e) => setNum2(e.target.value)} />
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+                >
+                <TextField id="outlined-basic" label="Second Number" variant="outlined" onChange={(e) => setNum2(e.target.value)} />
+            </Box>
+                {/* <input type='text' id='num' value={num2} placeholder='Second Number' onChange={(e) => setNum2(e.target.value)} /> */}
             </div>
-            <button id='equals' onClick={handleCalculation}> = </button>
-            <button id='reset' onClick={handleReset}>Reset</button>
+            <div>
+            <Stack style={{padding: 10}} spacing={2} direction="row">
+                <Button id='equals' onClick={handleCalculation} variant="contained">=</Button>
+            </Stack>
+            <Stack spacing={2} direction="row">
+                <Button id='reset' onClick={handleReset} variant="contained">Reset</Button>
+            </Stack>
+
+            {/* <button id='equals' onClick={handleCalculation}> = </button>
+            <button id='reset' onClick={handleReset}>Reset</button> */}
             <div>Result = {result} </div>
+            </div>
         </div>
     )
 }
